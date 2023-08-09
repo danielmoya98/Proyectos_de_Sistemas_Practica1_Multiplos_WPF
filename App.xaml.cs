c3f739b7774400b66c3dfa852de96965264cdaef
Daniel Moya <115199469+danielmoya98@users.noreply.github.com>
2023-08-09 18:38:39 -0400

    modificado ._.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,9 @@
 ﻿namespace Proyectos_de_Sistemas_Practica1
 {
-  /// <summary>
-  /// Interaction logic for App.xaml
-  /// </summary>
-  public partial class App
-  {
-  }
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,41 @@
-﻿namespace Proyectos_de_Sistemas_Practica1
+﻿using System;
+using System.Windows;
+
+namespace Proyectos_de_Sistemas_Practica1
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            int suma = 0;
+            string conteo = "";
+
+            for (int i = 1; i < Int32.Parse(longitud.Text) ; i++)
+            {
+                if (i % Int32.Parse(boxUno.Text) == 0 || i % Int32.Parse(boxDos.Text) == 0)
+                {
+                    conteo += i.ToString() + " ";
+                    suma += i;
+                }
+            }
+
+            this.suma.Text = suma.ToString();
+            this.conteo.Text = conteo;
+        }
+
+       
+
+        private void ButtonBase1_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
-  }
 }
