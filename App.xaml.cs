f130fecf206cd52d21bc408a3f0adae829817577
Daniel Moya <115199469+danielmoya98@users.noreply.github.com>
2023-08-09 18:35:28 -0400

    Con Rider ._.

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyectos_de_Sistemas_Practica1
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Proyectos_de_Sistemas_Practica1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
